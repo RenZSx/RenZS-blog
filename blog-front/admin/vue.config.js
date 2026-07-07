cd5afe61620819f3b4acb8afb37b57815b3a2908
chenfuyun <2956867099@qq.com>
2026-07-07 23:42:48 +0800

    调整前后端开发代理地址

--- blog-front/admin/vue.config.js
+++ blog-front/admin/vue.config.js
@@ -3,8 +3,8 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://8.137.86.224:8088/",
-        // target: "http://localhost:8088/",
+        // target: "http://8.137.86.224:8088/",
+        target: "http://localhost:8088/",
         changeOrigin: true,
         pathRewrite: {
           "^/api": ""
