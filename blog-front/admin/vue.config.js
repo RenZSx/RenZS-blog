6b1258661ecdebb80d9ba949132dbe02a55f2410
chenfuyun <2956867099@qq.com>
2026-08-12 15:44:46 +0800

    配置文件修改

--- blog-front/admin/vue.config.js
+++ blog-front/admin/vue.config.js
@@ -3,7 +3,6 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        // target: "http://8.137.86.224:8088/",
         target: "http://localhost:8088/",
         changeOrigin: true,
         pathRewrite: {
