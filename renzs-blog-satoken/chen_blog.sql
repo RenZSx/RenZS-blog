4bae5c06171a86429467ab982253966c8e0eeae0
chenfuyun <2956867099@qq.com>
2026-08-15 15:07:31 +0800

    chore:更新初始化sql

--- renzs-blog-satoken/chen_blog.sql
+++ renzs-blog-satoken/chen_blog.sql
@@ -1397,5 +1397,12 @@ CREATE TABLE `tb_website_config` (
 
 INSERT INTO `tb_website_config` (`id`, `config`, `create_time`, `update_time`) VALUES
     (1, '{}', '2026-08-13 14:54:33', '2026-08-13 14:54:35');
+INSERT  INTO tb_website_config (id, config, create_time, update_time)
+VALUES (
+  1,
+  '{"websiteAvatar":"","websiteName":"Renzs Blog","websiteAuthor":"Renzs","websiteIntro":"一个基于 Spring Boot 和 Vue 的博客","websiteNotice":"","websiteCreateTime":"","websiteVerse":"","websiteRecordNo":"","websitePoliceRecordNo":"","websiteBgAddress":"","socialLoginList":[],"socialUrlList":[],"qq":"","github":"","gitee":"","touristAvatar":"","userAvatar":"","isCommentReview":0,"isMessageReview":0,"isEmailNotice":0,"isEmailRegister":0,"isReward":0,"weiXinQRCode":"","alipayQRCode":"","articleCover":"","isChatRoom":0,"websocketUrl":"","isMusicPlayer":0,"isAiSummary":0,"aiApiUrl":"","aiApiKey":"","aiModel":"","aiApiType":"chat_completions","aiReasoningEffort":"","aiDisableResponseStorage":1,"aiSummaryPrompt":""}',
+  NOW(),
+  NOW()
+);
 
 SET FOREIGN_KEY_CHECKS = 1;
